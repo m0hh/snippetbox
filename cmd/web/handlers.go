55bf21682042435a49fa8827aa86236285097749
m0hh <lordegy53@gmail.com>
2023-10-23 22:07:53 +0300

    Added display of dynamic data to snippetview

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"text/template"
 
 	//"html/template"
 	"net/http"
@@ -58,7 +59,22 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	fmt.Fprintf(w, "%+v", snippet)
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/view.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	data := &templateData{Snippet: snippet}
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "snippetbox.mohamedehab.net/internal/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
