29abf2601f8004df420776d18613c85019fb0c1d
m0hh <lordegy53@gmail.com>
2023-11-09 14:20:58 +0200

    Added a flash message when creating a new snippet

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -14,6 +14,7 @@ type templateData struct {
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 	Form        any
+	Flash       string
 }
 
 func humanDate(t time.Time) string {
@@ -52,5 +53,5 @@ func newTemplateCache() (map[string]*template.Template, error) {
 }
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
-	return &templateData{CurrentYear: time.Now().Year()}
+	return &templateData{CurrentYear: time.Now().Year(), Flash: app.sessionManager.PopString(r.Context(), "flash")}
 }
