a140ccc9a82c2b85e56a7835f46dbe690ef82887
m0hh <lordegy53@gmail.com>
2023-11-10 15:21:43 +0200

    Added timeout configs to the server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -62,10 +62,13 @@ func main() {
 	}
 	tlsConfig := &tls.Config{CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256}}
 	srv := &http.Server{
-		Addr:      *addr,
-		ErrorLog:  errorLog,
-		Handler:   app.routes(),
-		TLSConfig: tlsConfig,
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
